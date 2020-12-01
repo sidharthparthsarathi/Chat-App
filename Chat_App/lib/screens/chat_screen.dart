a5c72c5c40f69c3ca4c71b7dfd04c28b273edf88
sidharthparthsarathi <sidharthparthsarathi051@gmail.com>
2020-12-01 16:59:47 +0530

    Added

---
+++ Chat_App/lib/screens/chat_screen.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          body: StreamBuilder(stream: Firestore.instance.collection('Chats/1vlw6Ymk05TeIPPsfD02/Messages').snapshots(),
+            builder: (ctx,streamSnapshot)
+            {
+              if(streamSnapshot.connectionState==ConnectionState.waiting)
+              {
+                return Center(child: CircularProgressIndicator());
+              }
+        final documents=streamSnapshot.data.documents;
+        return ListView.builder(
+        itemCount: documents.length,
+        itemBuilder: (ctx, index) => Container(
+        padding: EdgeInsets.all(8),
+        child: Text(documents[index]['text']),
+      ),
+      );
+            }), 
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),
+      onPressed:() {
+      } ,
+      ),
+    );
+  }
+}
